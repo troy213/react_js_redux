9bd5493a01d5104f59b2997251f1626d35238acd
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2021-03-13 16:06:12 +0700

    Multiple Rendering
    add Loading, and Error catch

--- src/components/page/Users.js
+++ src/components/page/Users.js
@@ -2,21 +2,50 @@ import React, { useState, useEffect } from 'react';
 
 const url = "https://api.github.com/users";
 
-
-
 const Users = (props) => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false)
 
-  const getUsers = async () => {
-    const response = await fetch(url);
-    const users = await response.json();
-    setUsers(users);
-  }
+  //--async - await fetching format--
+  // const getUsers = async () => {
+  //   const response = await fetch(url);
+  //   const users = await response.json();
+  //   setUsers(users);
+  // }
+  //
+  // useEffect(()=>{
+  //   getUsers();
+  // }, []);
 
   useEffect(()=>{
-    getUsers();
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+        }
+      })
+      .then((user) => {
+        setUsers(user);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err))
   }, []);
 
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return (
+      <>
+        <h2>404 Not Found</h2>
+        <p>Error...</p>
+      </>
+    );
+  }
   return(
     <>
       { users.map((value) => {
